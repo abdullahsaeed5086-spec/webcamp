fe7c72706d22a27e5b55483caa5a012cea3e5b7a
abdullah <you@example.com>
2025-08-28 11:38:17 +0500

    Childone in component

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,15 @@ import { useState } from 'react'
 import './App.css'
 import './components/webcamp.css';
 import WebCampPractice from './components/WebCampPractice'
+import Childone from './components/Childone';
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-     <WebCampPractice/>
+    <Childone/>
+     {/* <WebCampPractice/> */}
     </>
   )
 }
---
+++ src/components/Childone.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Childone = () => {
+  return (
+    <div>
+      <h1>ABDULLAH SAEED</h1>
+      <h2>ABDULLAH SAEED</h2>
+      <h3>ABDULLAH SAEED</h3>
+    </div>
+  )
+}
+
+export default Childone
